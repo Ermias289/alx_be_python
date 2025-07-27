796999af7f229dbdc91ecdb1963a05113dfc7618
Ermias289 <ermiasmolla00@gmail.com>
2025-07-27 16:17:46 +0300

    df

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,22 @@
+def perform_operation (num1, num2, operation):
+    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+    try:
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        
+        if operation == 'add':
+            result = num1 + num2
+        elif operation == 'subtract':
+            result = num1 - num2
+        elif operation == 'multiply':
+            result = num1 * num2
+        elif operation == 'divide':
+            if num2 == 0:
+                return "Error: Division by zero is not allowed."
+            result = num1 / num2
+        else:
+            return "Error: Invalid operation."
+
+        return f"The result of {operation}ing {num1} and {num2} is {result}."
+    except ValueError:
+        return "Error: Please enter valid numbers."
