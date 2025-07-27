0d686c36763a5459045b72a1094c0c8022c2bd43
Ermias289 <ermiasmolla00@gmail.com>
2025-07-27 18:02:58 +0300

    dgf

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,11 +1,11 @@
 weather = input("What's the weather like today? (sunny/rainy/cold): ").strip().lower()
 
 def provide_weather_advice(weather):
-    if weather == 'sunny':
+    if weather=='sunny':
         return "Wear a t-shirt and sunglasses."
-    elif weather == 'rainy':
+    elif weather=='rainy':
         return "Don't forget your umbrella and a raincoat."
-    elif weather == 'cold':
+    elif weather=='cold':
         return "Make sure to wear a warm coat and a scarf."
     else:
         return "Sorry, I don't have recommendations for this weather."
