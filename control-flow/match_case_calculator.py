fcad18bdb051468c8e03637eeedc3e40da66be60
Ermias289 <ermiasmolla00@gmail.com>
2025-07-27 18:25:44 +0300

    sfg

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -3,18 +3,19 @@ num2 = int(input("Enter the second number: "))
 operation = input("Choose the operation (+, -, *, /): ")
 
 def perform_operation(num1, num2, operation):
-    if operation == '+':
-        return num1 + num2
-    elif operation == '-':
-        return num1 - num2
-    elif operation == '*':
-        return num1 * num2
-    elif operation == '/':
-        if num2 == 0:
-            return "Error: Division by zero is not allowed."
-        return num1 / num2
-    else:
-        return "Error: Invalid operation."
+    match operation:
+        case '+':
+            return num1 + num2
+        case '-':
+            return num1 - num2
+        case '*':
+            return num1 * num2
+        case '/':
+            if num2 == 0:
+                return "Cannot divide by zero."
+            return num1 / num2
+        case _:
+            return "Error: Invalid operation."
 
 result = perform_operation(num1, num2, operation)
 print(f"The result is: {result}")
